3b1b82fd2c5f316657522d80766f0ca89e1421f7
Mendepsa <menderson.santos@aluno.epsa.com.br>
2023-06-08 11:23:06 -0300

    primeira-versao

---
+++ AppStoreTupinikim/AppStoreTupinikim/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppStoreTupinikim.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
